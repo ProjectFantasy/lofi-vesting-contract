af309c2761a705673fb49d1550949644d0f688a3
Toan Nguyen <blackntt@gmail.com>
2022-10-18 14:08:21 +0700

    fix: udpate script for generating vesting stages

--- scripts/stages.js
+++ scripts/stages.js
@@ -1,16 +1,49 @@
 const { BN } = require('bn.js')
-const { generateMonthlyVestingStagesInWei } = require('../utils/generate-vesting-stage')
+const { generateMonthlyVestingStages, generateMonthlyVestingStagesInWei } = require('../utils/generate-vesting-stage')
 const moment = require('moment')
-generateMonthlyVestingStagesInWei(new BN(300000000), new BN(7 * 12), new BN(Math.floor(Date.now() / 1000))).then(
-  ({ vestingTimes, vestingAmounts }) => {
-    console.log(vestingTimes.map((v) => v.toString()))
-    console.log(vestingTimes.map((v) => moment.unix(v).format('DD-MM-YYYY HH:mm:ss')))
-    console.log(vestingAmounts.map((v) => v.toString()))
-  }
-)
+const fs = require('fs')
 
-// vestLogs.push({
-//   time: moment.unix(vestingTime).format('DD-MM-YYYY HH:mm:ss'),
-//   timestamp: vestingTime.toString(),
-//   amount: web3.utils.fromWei(vestingAmount, 'ether'),
-// })
+const generateStagesInWei = () => {
+  generateMonthlyVestingStagesInWei(new BN(300000000), new BN(7 * 12), new BN(Math.floor(Date.now() / 1000))).then(
+    ({ vestingTimes, vestingAmounts }) => {
+      console.log(vestingTimes.map((v) => v.toString()))
+      console.log(vestingTimes.map((v) => moment.unix(v).format('DD-MM-YYYY HH:mm:ss')))
+      console.log(vestingAmounts.map((v) => v.toString()))
+    }
+  )
+}
+
+const generateStages = (totalAmount, times, file) => {
+  generateMonthlyVestingStages(
+    totalAmount,
+    times,
+    moment
+      .unix(new BN(Math.floor(Date.now() / 1000)))
+      .add(10, 'days')
+      .unix()
+  ).then(({ vestingTimes, vestingAmounts }) => {
+    let content = ''
+    content = content + 'Date time,Timestamp,Amount' + '\n'
+    for (let i = 0; i < vestingTimes.length; i++) {
+      const time = vestingTimes[i]
+      const amount = vestingAmounts[i]
+      // console.log(moment.unix(time).format('DD-MM-YYYY HH:mm:ss') + ',' + time.toString() + ',' + amount.toString())
+
+      content =
+        content +
+        moment.unix(time).format('DD-MM-YYYY HH:mm:ss') +
+        ',' +
+        time.toString() +
+        ',' +
+        amount.toString() +
+        '\n'
+    }
+    fs.writeFileSync(file, content)
+  })
+}
+generateStages(new BN(300000000), new BN(7 * 12), './notes/love_2_earn.csv')
+generateStages(new BN(280000000), new BN(7 * 12), './notes/marketing.csv')
+generateStages(new BN(100000000), new BN(1 * 12), './notes/sale.csv')
+generateStages(new BN(220000000), new BN(5 * 12), './notes/team.csv')
+generateStages(new BN(50000000), new BN(3 * 12), './notes/advisor.csv')
+generateStages(new BN(50000000), new BN(7 * 12), './notes/reserved.csv')
